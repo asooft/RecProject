0a62ad201a64af87d1346ff8e4ebff21fd4893ce
asooft <arabie@mu.edu.eg>
2023-06-23 02:22:44 +0300

    Update app.py

--- app.py
+++ app.py
@@ -268,7 +268,7 @@ with st.container():
         """
         <style>
         .rectangle-box {
-            background-color: #D1F1D0;
+            background-color: #D1F1D0 important!;
             border: 1px solid #d3d3d3;
             border-radius: 5px;
             padding: 10px;
