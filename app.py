bdda1704b47aed805fdcddfacaf5a93f8108adf6
asooft <arabie@mu.edu.eg>
2023-06-23 16:48:48 +0300

    edit

--- app.py
+++ app.py
@@ -388,7 +388,8 @@ def run_movie_based():
             st.write(row['title'])
         
     # Create a selectbox to choose a movie
-    selected_movie = st.radio("Select a movie", movies['title'].unique())
+    with st.beta_expander("Select a movie"):
+        selected_movie = st.selectbox("Choose a movie", movies['title'].unique(), index=0)
 
     # Filter the DataFrame based on the selected movie
     #filtered_movies = movies[movies['title'] == selected_movie]
