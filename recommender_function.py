62a9c32535eb9d8d0e5b610e6e53539dc1cc6933
asooft <arabie@mu.edu.eg>
2023-06-27 16:25:03 +0300

    edit

--- recommender_function.py
+++ recommender_function.py
@@ -57,13 +57,7 @@ def get_recommendations(movies_data,df,user_id, k):
       #print("Current path:", current_path)
       #st.write("Current path:", current_path)
       
-      folder_path = 'Col'  # Specify the folder path
 
-        # Iterate over each file in the folder
-        for file_name in os.listdir(folder_path):
-            # Check if the path is a file (not a subdirectory)
-            if os.path.isfile(os.path.join(folder_path, file_name)):
-                print(file_name)
       
       new_model = RecommenderNet()
       options = tf.saved_model.LoadOptions(experimental_io_device='/job:localhost')
