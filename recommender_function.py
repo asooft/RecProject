5990e3fb6bdb19a9bc33f7a067c3d8a5141b4ade
asooft <arabie@mu.edu.eg>
2023-06-27 16:58:59 +0300

    edit

--- recommender_function.py
+++ recommender_function.py
@@ -69,7 +69,7 @@ def get_recommendations(movies_data,df,user_id, k):
     options = tf.saved_model.LoadOptions(experimental_io_device='/job:localhost')
     #new_model = tf.keras.models.load_model('Col', options=options)
     model_path = "Col/saved_model.pb"  # Path to the saved_model.pb file
-    model = tf.keras.models.load_model(model_path)    
+    model = tf.keras.models.load_model('')    
     #model=keras.models.load_model("")
     #imported = tf.saved_model.load(path)
     #model = model.signatures["fingerprint.pb"]
